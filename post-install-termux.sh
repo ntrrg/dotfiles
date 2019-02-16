ee5a3a6b816d85426192d4ec2c2e569eb70e6a9a
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2019-02-16 15:48:14 -0700

    post-install: Add Termux script

---
+++ post-install-termux.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+
+apt update
+apt upgrade -y
+
+apt install -y \
+  elinks \
+  git \
+  golang \
+  htop \
+  jq \
+  make \
+  mosh \
+  openssh \
+  pv \
+  rsync \
+  screen \
+  termux-api \
+  vim \
+  wget \
+  zsh
+
+mkdir "$HOME/bin"
+
+# ET
+
+if [ ! -f et.sh ]; then
+  wget 'https://gist.githubusercontent.com/ntrrg/1dbd052b2d8238fa07ea5779baebbedb/raw/371724030a77113a621fbb7f43b5be506f2eb18d/et.sh'
+fi
+
+cp -f et.sh "$HOME/bin/et"
+chmod +x "$HOME/bin/et"
+
+# Urchin
+
+if [ ! -f urchin-v0.1.0-rc3 ]; then
+  wget -O urchin-v0.1.0-rc3 'https://raw.githubusercontent.com/tlevine/urchin/v0.1.0-rc3/urchin'
+fi
+
+cp -f urchin-v0.1.0-rc3 "$HOME/bin/urchin"
+chmod +x "$HOME/bin/urchin"
+
