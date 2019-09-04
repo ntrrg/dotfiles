fbd4c3317ad668861d0570a6552f81bb2b3543b2
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2019-09-04 14:17:09 -0400

    bin: Add read_secret script

---
+++ bin/read_secret.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright (c) 2019 Miguel Angel Rivera Notararigo
+# Released under the MIT License
+
+enable_echo() {
+  stty echo
+}
+
+trap enable_echo EXIT
+stty -echo
+IFS= read PASSWD
+enable_echo
+trap - EXIT
+echo "$PASSWD"
+
