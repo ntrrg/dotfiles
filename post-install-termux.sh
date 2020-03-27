6a8a9c6e1bb8038b34dcc8a41ea1ffbdcbcb494a
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2020-03-27 19:19:29 -0400

    post-install: Remove language specific tools

--- post-install-termux.sh
+++ post-install-termux.sh
@@ -13,13 +13,11 @@ apt install -y \
   bc \
   elinks \
   git \
-  golang \
   htop \
   jq \
   make \
   mosh \
   openssh \
-  protobuf \
   pv \
   rsync \
   screen \
@@ -28,21 +26,6 @@ apt install -y \
   vim \
   wget \
   zsh
-
-# Go
-
-mkdir -p "$PREFIX/lib/go/bin/"
-ln -sf "$PREFIX/bin/go*" "$PREFIX/lib/go/bin/"
-
-# Urchin
-
-if [ ! -f urchin-v0.1.0-rc3 ]; then
-  wget -O urchin-v0.1.0-rc3 'https://raw.githubusercontent.com/tlevine/urchin/v0.1.0-rc3/urchin'
-fi
-
-mkdir -p "$HOME/.local/bin"
-mv urchin-v0.1.0-rc3 "$HOME/.local/bin/urchin"
-chmod +x "$HOME/.local/bin/urchin"
 
 ############
 # Cleaning #
