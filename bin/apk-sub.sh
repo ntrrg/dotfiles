19f81c190c460a9cc601edf2a74a958b8e691251
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2021-03-02 18:26:39 -0400

    bin/apk-sub.sh: Fix bad arguments parsing

--- bin/apk-sub.sh
+++ bin/apk-sub.sh
@@ -4,21 +4,18 @@
 
 set -e
 
-PACKAGES="${@:-"$(apk info | sort)"}"
-
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	BIN_NAME="$(basename "$0")"
 
 	cat << EOF
 $BIN_NAME - find Alpine subpackages.
 
-Usage: $BIN_NAME -s SUFFIX [PACKAGE]...
+Usage: $BIN_NAME SUFFIX [PACKAGE]...
 
-Find subpackages for given packages, if no packages are given, the full list of
-installed packages will be used.
+Find subpackages for given packages with the given suffix, if no packages are
+given, the full list of installed packages will be used.
 
 Options:
-  -s, --suffix=SUFFIX   Look for subpackages with the given suffix
   -h, --help            Show this help message
 
 Copyright (c) 2021 Miguel Angel Rivera Notararigo
@@ -28,15 +25,18 @@ EOF
 	exit
 fi
 
-if [ -z "$1" ]; then
+if [ $# -eq 0 ]; then
 	echo "no suffix provided" > "/dev/stderr"
 	exit 1
 fi
 
 SUFFIX="$1"
-ALL_PKGS="$(apk search "*")"
+shift
 
-for PKG in $PACKAGES; do
+ALL_PKGS="$(apk search "*")"
+PKGS="${@:-"$(apk info | sort)"}"
+
+for PKG in $PKGS; do
 	SUBPKG="$PKG-$SUFFIX"
 
 	if echo "$ALL_PKGS" | grep -q "^$SUBPKG-\d"; then
