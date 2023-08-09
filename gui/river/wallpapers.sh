7dde2539b53a113a343d2962d79334c235b926e7
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2023-08-09 16:05:21 -0400

    gui/river: Don't check if River is running

--- gui/river/wallpapers.sh
+++ gui/river/wallpapers.sh
@@ -8,8 +8,7 @@ _RIVER_WALLPAPERS_INTERVAL="${RIVER_WALLPAPERS_INTERVAL:-"30s"}"
 while true; do
 	for _wallpaper in $_RIVER_WALLPAPERS/*; do
 		pkill swaybg || true
-		ps -a | grep -q '\sriver$' || log.sh -f "river is not running"
-		riverctl spawn "swaybg --output '*' --mode fill --image '$_wallpaper'" 2> /dev/null
+		swaybg --output "*" --mode fill --image "$_wallpaper" &
 		sleep $_RIVER_WALLPAPERS_INTERVAL
 	done
 done
