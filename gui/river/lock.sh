dcf308dd28c970ec58deb7fada3b8be584f59663
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2024-09-17 20:08:33 -0400

    gui/river: Fix lockscreen wallpapers path

--- gui/river/lock.sh
+++ gui/river/lock.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-_RIVER_WALLPAPERS="${RIVER_WALLPAPERS:-"$HOME/Pictures/Wallpapers/Landscape"}"
+_RIVER_WALLPAPERS="${RIVER_WALLPAPERS:-"$HOME/Pictures/Wallpapers/Horizontal"}"
 
 exec swaylock -c 000000 -F -k -l -s fill -i "$(find "$_RIVER_WALLPAPERS" | shuf -n 1)"
